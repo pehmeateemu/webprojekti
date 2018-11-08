27d912d6191a706b4c980b9ad7280c0f52171720
Teemu Pietiläinen <Teemu.Pietilainen@edu.savonia.fi>
2018-11-08 11:59:26 +0200

    ui säädöt 1

--- login.php
+++ login.php
@@ -8,12 +8,14 @@
 	
 	<p>Kirjaudu sisään järjestelmään</p>
 	<form action="login_handler.php" method="post">
-		<label for="tunnus">Tunnus</label>
-		<input type="text" name="tunnus" /> 
-		<label for="ss">Tunnus</label>
-		<input type="text" name="ss" /> 
-		
-		<input type="submit" name="login" value="Kirjaudu" />
+		<table>
+		<tr>
+		<td><label for="tunnus">Tunnus</label></td>
+		<td><input type="text" name="tunnus" /></td> </tr>
+		<tr>
+		<td><label for="ss">Salasana</label></td>
+		<td><input type="text" name="ss" /> </td>
+		<td><input type="submit" name="login" value="Kirjaudu" /></td></tr>	
 	</form>
 
 </body>
